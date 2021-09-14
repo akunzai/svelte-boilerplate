7205b85293b5af7195a3996e8fd1207dcfd434f7
Charley Wu <akunzai@gmail.com>
2021-09-14 17:50:54 +0800

    Simplify Svelte tests with svelte-htm
    - https://github.com/kenoxa/svelte-htm

--- src/common/NavMenu.test.ts
+++ src/common/NavMenu.test.ts
@@ -1,9 +1,15 @@
 import { fireEvent, render, screen } from '@testing-library/svelte';
+import html from 'svelte-htm';
+import { Router } from 'svelte-navigator';
 import '../i18nForTests';
-import NavMenu from './NavMenu.test.svelte';
+import NavMenu from './NavMenu.svelte';
 
 beforeEach(() => {
-  render(NavMenu, {title: 'Test'});
+  render(html`
+    <${Router}>
+      <${NavMenu} title=${'Test'} />
+    <//>
+  `);
 });
 
 test('should render with title: Test', () => {
@@ -13,15 +19,21 @@ test('should render with title: Test', () => {
 test('support to toggle navigation', async () => {
   const navbar = screen.getByTestId('navbar-collapse');
   expect(navbar.getAttribute('class')).not.toContain('show');
-  await fireEvent.click(screen.getByRole('button', { name: /Toggle navigation/i }));
+  await fireEvent.click(
+    screen.getByRole('button', { name: /Toggle navigation/i })
+  );
   expect(navbar.getAttribute('class')).toContain('show');
 });
 
 test('support to switch languages', async () => {
-  await fireEvent.click(screen.getByRole('button', { name: /Toggle Languages/i }));
+  await fireEvent.click(
+    screen.getByRole('button', { name: /Toggle Languages/i })
+  );
   await fireEvent.click(screen.getByRole('button', { name: /English/i }));
   expect(localStorage.getItem('locale')).toBe('en');
-  await fireEvent.click(screen.getByRole('button', { name: /Toggle Languages/i }));
+  await fireEvent.click(
+    screen.getByRole('button', { name: /Toggle Languages/i })
+  );
   await fireEvent.click(screen.getByRole('button', { name: /正體中文/i }));
   expect(localStorage.getItem('locale')).toBe('zh-Hant');
 });
