428c141127c16670bd900bdc733b4a320f275f06
Charley Wu <akunzai@gmail.com>
2021-09-19 22:02:47 +0800

    Improve routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { Route,Router } from 'svelte-navigator';
+  import { Route, Router } from 'svelte-navigator';
+  import Counter from './components/Counter.svelte';
   import Home from './components/Home.svelte';
   import NavMenu from './components/NavMenu.svelte';
-  import Counter from './components/Counter.svelte';
   import TodoDetail from './components/TodoDetail.svelte';
   import TodoList from './components/TodoList.svelte';
 </script>
@@ -10,9 +10,9 @@
 <Router primary={false}>
   <NavMenu title={document.title} />
   <main class="container">
-    <Route component={Home}/>
     <Route path="/counter" component={Counter} />
     <Route path="/todo-list" component={TodoList} />
     <Route path="/todo/:id" component={TodoDetail} />
+    <Route component={Home} />
   </main>
 </Router>
