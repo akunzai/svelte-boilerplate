449874978f75f99666b5a60cb68054acd52e7c42
Charley Wu <akunzai@gmail.com>
2023-10-23 22:56:06 +0800

    Fix ERROR: Top-level await is not available in the configured target environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [svelte()],
   server: {
     open: true,
